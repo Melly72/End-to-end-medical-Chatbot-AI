457ece7a91033b5770a0d14344323c71cf1adb2d
Melly72 <manumelly72@gmail.com>
2025-01-25 10:19:17 +0300

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical AI BOT',
+    version= '0.0.0',
+    author= 'Manu Melly',
+    author_email= 'manumelly72@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
